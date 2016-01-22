cb9c31709121d94ea5017bc714bc3c30725dd74e
Sujesh Kumar UV <sujeshkumaruv2@gmail.com>
2016-01-22 14:25:40 +0530

    Initialize Repo

---
+++ middleware.js
@@ -0,0 +1,12 @@
+var middleware = {
+	requireAuthentication : function (req,res,next) {
+		console.log('Private route hit');
+		next();
+	},
+	logger: function (req,res,next) {
+		console.log('Request:' + new Date().toString()+ ' ' + req.method +' '+ req.orginalUri);
+		next();
+	}
+}
+
+module.exports = middleware;
